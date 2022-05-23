84a26998acfe5f911655a6aee891fc200bb7159b
DanielAvilez1 <104475313+DanielAvilez1@users.noreply.github.com>
2022-05-22 21:33:44 -0700

    finish

--- calculator.js
+++ calculator.js
@@ -1,21 +1,28 @@
 function calculation(){
     
-    let num1 =prompt("Enter the num1: ");
-    let num2 =prompt("Enter the num2: ");
-    sum(num1,num2);
+    let num1 =Number (prompt("Enter the num1: "));
+    let num2 =Number (prompt("Enter the num2: "));
+    
     document.getElementById("results").
     innerHTML=`
     <p> ${num1} + ${num2} = ${sum(num1,num2)} </p>
     <p> Sub ${num1} - ${num2} = ${subtraction(num1,num2)}</p>
-    <p> Multi ${num1} * ${num2} = ${sum(num1,num2)}</p>
-    <p> Div ${num1} / ${num2} = ${sum(num1,num2)}</p>
+    <p> Multi ${num1} * ${num2} = ${multi(num1,num2)}</p>
+    <p> Div ${num1} / ${num2} = ${div(num1,num2)}</p>
     `;
 }
 
-function sum(a+b){
+function sum(a,b){
     return a+b;
-    return a-b;
-    return a*b;
+
 
 }
-function subtraction(a-b)
+function subtraction(a,b){
+    return a-b;
+}
+function multi(a,b){
+    return a*b;
+}
+function div(a,b){
+    return a/b;
+}
